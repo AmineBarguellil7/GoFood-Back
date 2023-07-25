ef30fd39f6db28eb0c30e55323a746e5cb4c846d
Amine <amine.barguellil@esprit.tn>
2023-07-25 17:24:17 +0100

    login🚀

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,24 +1,73 @@
-const express = require('express')
-const Router=express.Router()
-const User=require("../models/User")
+const express = require("express");
+const Router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-Router.post('/addUser',async(req,res)=> {
+Router.post(
+  "/addUser",
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 5 }),
+    body("password", "incorrect password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
     try {
-        await User.create(
-            {
-                name:"amine",
-                password:"amine",
-                email:"amine@gmail.com",
-                location:"Tunis"
-            }
-        )
-        res.json({success:true})
-    }
-    catch (error) {
-        console.log(error);
-        res.json({success:false})
-    }
-})
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
+Router.post(
+  "/loginuser",
+  [
+    body("email","incorrect email").isEmail(),
+    body("password", "incorrect password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    let email = req.body.email;
+    try {
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      let userData = await User.find({email:email});
+
+
+
+      if (userData[0].email !==email) {
+        return res
+          .status(400)
+          .json({ errors: "try logging with correct email" });
+      }
+
+      if (req.body.password !== userData[0].password) {
+        return res
+          .status(400)
+          .json({ errors: "try logging with correct password" });
+      }
+
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
 
 module.exports = Router;
