947dcca8bacccd90f2e2e7c87603b6e7fa6fcb63
Amine <amine.barguellil@esprit.tn>
2023-07-26 11:48:24 +0100

    jwt🚀

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -3,6 +3,11 @@ const Router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
+
+const bcrypt = require('bcryptjs');
+const jwt=require("jsonwebtoken");
+const jwtSecret="Hello amine";
+
 Router.post(
   "/addUser",
   [
@@ -11,6 +16,13 @@ Router.post(
     body("password", "incorrect password").isLength({ min: 5 }),
   ],
   async (req, res) => {
+
+
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const secPassword = await bcrypt.hash(req.body.password, salt);
+
+
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -19,7 +31,7 @@ Router.post(
 
       await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPassword,
         email: req.body.email,
         location: req.body.location,
       });
@@ -49,20 +61,29 @@ Router.post(
       let userData = await User.find({email:email});
 
 
-
-      if (userData[0].email !==email) {
+      if (userData.length===0) {
         return res
           .status(400)
-          .json({ errors: "try logging with correct email" });
+          .json({ errors: "there is no user with this email address" });
       }
 
-      if (req.body.password !== userData[0].password) {
+      const pwdCompare=bcrypt.compare(req.body.password,userData[0].password)
+
+      if (!pwdCompare) {
         return res
           .status(400)
           .json({ errors: "try logging with correct password" });
       }
 
-      res.json({ success: true });
+      const data={
+        user:{
+          id:userData[0]._id
+        }
+      }
+
+      const authToken=jwt.sign(data,jwtSecret);
+
+      res.json({ success: true ,authToken:authToken});
     } catch (error) {
       console.log(error);
       res.json({ success: false });
